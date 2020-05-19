e05919b00ba87c488ac587eb0a9e03492dd07d10
imoralesc <i.moralesc@outlook.es>
2020-05-20 01:28:23 +0200

    firebase
    Signed-off-by: imoralesc <i.moralesc@outlook.es>

--- aplicacion/src/app/app.module.ts
+++ aplicacion/src/app/app.module.ts
@@ -9,10 +9,26 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD-lRYUYlIladR3XL8uQj5A5o26iqAkFLo",
+  authDomain: "admyfin-cb8f1.firebaseapp.com",
+  databaseURL: "https://admyfin-cb8f1.firebaseio.com",
+  projectId: "admyfin-cb8f1",
+  storageBucket: "admyfin-cb8f1.appspot.com",
+  messagingSenderId: "785083156813",
+  appId: "1:785083156813:web:7eb557ab64c06eaee64f04"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule],
   providers: [
     StatusBar,
     SplashScreen,
